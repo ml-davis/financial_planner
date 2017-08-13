6cff201dc1b1a80b48dcbc1f334697c1ac225e91
Matthew Davis <ml.davis195@gmail.com>
2017-08-12 21:50:11 -0400

    Removed load to json since it's no longer needed.

--- IOHandler.C
+++ IOHandler.C
@@ -3,34 +3,6 @@
 using namespace std;
 using namespace boost::gregorian;
 
-void IOHandler::savePartitionsToJSON(unordered_map<string, Partition> partitions)
-{
-  ofstream outputStream;
-  outputStream.open ("partitions.json");
-
-  outputStream << "{\n";
-  size_t index = 0;
-  for (auto p: partitions)
-  {
-    outputStream << "\t\"" << p.first << "\": {\n";
-    outputStream << "\t\t\"description\": \"" << p.second.getDescription() << "\",\n";
-    outputStream << "\t\t\"dueDate\": " << p.second.getDueDate() << ",\n";
-    outputStream << "\t\t\"amount\": " << p.second.getAmount() << "\n";
-
-    if (++index < partitions.size())
-    {
-      outputStream << "\t},\n";
-    }
-    else
-    {
-      outputStream << "\t}\n";
-    }
-  }
-  outputStream << "}";
-
-  outputStream.close();
-}
-
 void IOHandler::savePartitions(const unordered_map<string, Partition>& partitions)
 {
   ofstream outputStream;
